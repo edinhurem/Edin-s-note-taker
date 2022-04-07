339ae889c24090b054eecd80c31e137ea2d207ed
Edinh <edinhurem@gmail.com>
2022-04-06 20:46:54 -0400

    setup

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-// Dependenceis 
 const { text } = require('express');
 const express = require('express');
 const res = require('express/lib/response');
@@ -6,18 +5,14 @@ const { readFileSync } = require('fs');
 const path = require('path');
 const db = require('./db/db.json')
 const app = express();
-const fs = require('fs');
-
-// Port
 const PORT = process.env.PORT || 3001;
+
 
 app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({
   extended: true
 }));
-
-//API Routes
 
 app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, '/public/notes.html'));
@@ -42,19 +37,18 @@ app.post('/api/notes', (req, res) => {
     };
 
     console.log(response);
-    res.status(201).json(response);
+    res.status(201).json(resposne);
   } else {
     res.status(500).json('Error in posting notes');
   }
 });
 
+app.delete('/:id', (req, res) => res.json('DELETE route'));
 
-// HTML ROUTES
+
 app.get('*', (req, res,) => {
   res.sendFile(path.join(__dirname, '/public/index.html'));
 })
-
-// Listening
 app.listen(PORT, () =>
   console.log(`Example app listening at http://localhost:${PORT}`)
 );
